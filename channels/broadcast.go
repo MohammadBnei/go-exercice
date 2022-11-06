e988a135cfa4d3cdce8fb4da3df7f6293c0c42bc
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-11-06 18:01:22 +0100

    feat(broadcaster): implementation and test ok

---
+++ channels/broadcast.go
@@ -0,0 +1,82 @@
+package channels
+
+type Broadcaster interface {
+	// Register a new channel to receive broadcasts
+	Register(chan<- interface{})
+	// Unregister a channel so that it no longer receives broadcasts.
+	Unregister(chan<- interface{})
+	// Shut this broadcaster down.
+	Close() error
+	// Submit a new object to all subscribers
+	Submit(interface{}) bool
+}
+
+type broadcaster struct {
+	input chan interface{}
+	reg   chan chan<- interface{}
+	unreg chan chan<- interface{}
+
+	outputs map[chan<- interface{}]bool
+}
+
+func (b *broadcaster) broadcast(m interface{}) {
+	for ch := range b.outputs {
+		ch <- m
+	}
+}
+
+func (b *broadcaster) run() {
+	for {
+		select {
+		case m := <-b.input:
+			b.broadcast(m)
+		case ch, ok := <-b.reg:
+			if ok {
+				b.outputs[ch] = true
+			} else {
+				return
+			}
+		case ch := <-b.unreg:
+			delete(b.outputs, ch)
+		}
+	}
+}
+
+func NewBroadcaster(buflen int) Broadcaster {
+	b := &broadcaster{
+		input:   make(chan interface{}, buflen),
+		reg:     make(chan chan<- interface{}),
+		unreg:   make(chan chan<- interface{}),
+		outputs: make(map[chan<- interface{}]bool),
+	}
+
+	go b.run()
+
+	return b
+}
+
+func (b *broadcaster) Register(newch chan<- interface{}) {
+	b.reg <- newch
+}
+
+func (b *broadcaster) Unregister(ch chan<- interface{}) {
+	b.unreg <- ch
+}
+
+func (b *broadcaster) Close() error {
+	close(b.reg)
+	close(b.unreg)
+	return nil
+}
+
+func (b *broadcaster) Submit(m interface{}) bool {
+	if b == nil {
+		return false
+	}
+	select {
+	case b.input <- m:
+		return true
+	default:
+		return false
+	}
+}
---
+++ channels/broadcast_test.go
@@ -0,0 +1,155 @@
+package channels_test
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/MohammadBnei/go-exercice/channels"
+)
+
+func TestBroadcaster(t *testing.T) {
+	bc := channels.NewBroadcaster(10)
+	defer bc.Close()
+
+	messages := make(chan interface{})
+	messages2 := make(chan interface{})
+
+	bc.Register(messages)
+	bc.Register(messages2)
+
+	var wg sync.WaitGroup
+
+	wg.Add(4)
+	go func(msg <-chan interface{}) {
+		i := 1
+		for m := range msg {
+
+			sm, ok := m.(string)
+			if !ok {
+				t.Errorf("wrong type passed to message channel, expected %T got %T", "", sm)
+				return
+			}
+			if sm != fmt.Sprintf("test%v", i) {
+				t.Errorf("wrong messages, expected %s got %s", fmt.Sprintf("test%v", i), sm)
+			}
+			i++
+			wg.Done()
+		}
+		if i != 3 {
+			t.Errorf("expected %v run, got %v", 3, i)
+		}
+	}(messages)
+
+	go func(msg <-chan interface{}) {
+		i := 1
+		for m := range msg {
+
+			sm, ok := m.(string)
+			if !ok {
+				t.Errorf("wrong type passed to message channel, expected %T got %T", "", sm)
+				return
+			}
+			if sm != fmt.Sprintf("test%v", i) {
+				t.Errorf("wrong messages, expected %s got %s", fmt.Sprintf("test%v", i), sm)
+			}
+			i++
+			wg.Done()
+		}
+		if i != 4 {
+			t.Errorf("expected %v run, got %v", 4, i)
+		}
+	}(messages2)
+
+	bc.Submit("test1")
+	bc.Submit("test2")
+	wg.Wait()
+
+	wg.Add(1)
+	bc.Unregister(messages)
+	bc.Submit("test3")
+	wg.Wait()
+
+}
+
+func TestClosedBroadcaster(t *testing.T) {
+	bc := channels.NewBroadcaster(10)
+
+	messages := make(chan interface{})
+
+	bc.Register(messages)
+	bc.Close()
+
+	if !bc.Submit("test closed") {
+		t.Error("should return false, returned true")
+	}
+}
+
+func TestMultipleBroadcaster(t *testing.T) {
+	bc1 := channels.NewBroadcaster(10)
+
+	messages1 := make(chan interface{})
+
+	bc1.Register(messages1)
+	defer bc1.Close()
+
+	bc2 := channels.NewBroadcaster(10)
+
+	messages2 := make(chan interface{})
+
+	bc2.Register(messages2)
+	defer bc2.Close()
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func(msg <-chan interface{}) {
+		defer wg.Done()
+		for i := 0; i < 3; i++ {
+			m, ok := <-msg
+			if !ok {
+				break
+			}
+			sm, ok := m.(string)
+			if !ok {
+				t.Errorf("wrong type passed to message channel, expected %T got %T", "", sm)
+				return
+			}
+			if sm != fmt.Sprintf("bc1 %v", i+1) {
+				t.Errorf("wrong messages, expected %s got %s", fmt.Sprintf("bc1 %v", i+1), sm)
+
+			}
+		}
+	}(messages1)
+
+	go func(msg <-chan interface{}) {
+		defer wg.Done()
+		for i := 0; i < 3; i++ {
+			m, ok := <-msg
+			if !ok {
+				break
+			}
+			sm, ok := m.(string)
+			if !ok {
+				t.Errorf("wrong type passed to message channel, expected %T got %T", "", sm)
+				return
+			}
+			if sm != fmt.Sprintf("bc2 %v", i+1) {
+				t.Errorf("wrong messages, expected %s got %s", fmt.Sprintf("bc2 %v", i+1), sm)
+
+			}
+
+		}
+	}(messages2)
+
+	bc1.Submit("bc1 1")
+	bc1.Submit("bc1 2")
+	bc1.Submit("bc1 3")
+
+	bc2.Submit("bc2 1")
+	bc2.Submit("bc2 2")
+	bc2.Submit("bc2 3")
+
+	wg.Wait()
+}
