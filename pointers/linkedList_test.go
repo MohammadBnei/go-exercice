ffef084a50c0e77004acba3dc80b3dcac09bd7c2
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-11-07 12:51:40 +0100

    refactor(pointers): removed log followed by fail and used errorf

--- pointers/linkedList_test.go
+++ pointers/linkedList_test.go
@@ -15,8 +15,7 @@ func TestSizeAndPush(t *testing.T) {
 	size, _ := list.Size()
 
 	if size != 2 {
-		t.Logf("Error on size. Should have 2 elements, have %v", size)
-		t.Fail()
+		t.Errorf("Error on size. Should have 2 elements, have %v", size)
 	}
 }
 
@@ -31,17 +30,14 @@ func TestPushAndGet(t *testing.T) {
 	errNode, err := list.Get(33)
 
 	if hey.Data != "hey" {
-		t.Logf("Error on get. Should have \"hey\", have %v", hey.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have \"hey\", have %v", hey.Data)
 	}
 	if second.Data != 123 {
-		t.Logf("Error on get. Should have 123, have %v", second.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have 123, have %v", second.Data)
 	}
 
 	if err == nil {
-		t.Logf("Error on get. Should have an out of bounds error, have %v", errNode)
-		t.Fail()
+		t.Errorf("Error on get. Should have an out of bounds error, have %v", errNode)
 	}
 }
 
@@ -58,33 +54,27 @@ func TestInsert(t *testing.T) {
 	numbers, _ := list.Get(0)
 
 	if mia.Data != "mamamia" {
-		t.Logf("Error on get. Should have \"mamamia\", have %v", mia.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have \"mamamia\", have %v", mia.Data)
 	}
 	if hey.Data != "hey" {
-		t.Logf("Error on get. Should have \"hey\", have %v", hey.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have \"hey\", have %v", hey.Data)
 	}
 	if numbers.Data != 123 {
-		t.Logf("Error on get. Should have 123, have %v", numbers.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have 123, have %v", numbers.Data)
 	}
 
 	err := list.Insert("luigi", 4)
 	if err == nil {
 		t.Log("Error on insert. Should return an error on index 4 out of bound")
-		t.Fail()
 	}
 	list.Insert("luigi", 3)
 	if mario, _ := list.Get(3); mario.Data != "luigi" {
-		t.Logf("Error on insert. Should have 123, have %v", mario.Data)
-		t.Fail()
+		t.Errorf("Error on insert. Should have 123, have %v", mario.Data)
 	}
 
 	list.Insert("henry", 0)
 	if arsenal, _ := list.Get(0); arsenal.Data != "henry" {
-		t.Logf("Error on insert. Should have henry, have %v", arsenal.Data)
-		t.Fail()
+		t.Errorf("Error on insert. Should have henry, have %v", arsenal.Data)
 	}
 
 }
@@ -100,11 +90,9 @@ func TestSearch(t *testing.T) {
 	found, err := list.Search(2)
 	if err != nil {
 		t.Log("Error on search. Should have found node with data 2")
-		t.Fail()
 	}
 	if found.Data != 2 {
-		t.Logf("Error on search. Should have 2, have %v", found.Data)
-		t.Fail()
+		t.Errorf("Error on search. Should have 2, have %v", found.Data)
 	}
 }
 
@@ -121,36 +109,30 @@ func TestRemove(t *testing.T) {
 	numbers, _ := list.Get(0)
 
 	if found, err := list.Search(removed.Data); err == nil {
-		t.Logf("Error on removal. Should not have a node with data %s", found.Data)
-		t.Fail()
+		t.Errorf("Error on removal. Should not have a node with data %s", found.Data)
 	}
 
 	if hey.Data != "hey" {
-		t.Logf("Error on get. Should have \"hey\", have %v", hey.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have \"hey\", have %v", hey.Data)
 	}
 	if numbers.Data != 123 {
-		t.Logf("Error on get. Should have 123, have %v", numbers.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have 123, have %v", numbers.Data)
 	}
 
 	list.Push("mamamia")
 	removed, _ = list.Remove(2)
 
 	if found, err := list.Search(removed.Data); err == nil {
-		t.Logf("Error on removal. Should not have a node with data %s", found.Data)
-		t.Fail()
+		t.Errorf("Error on removal. Should not have a node with data %s", found.Data)
 	}
 
 	numbers, _ = list.Get(1)
 	mamamia, _ := list.Get(0)
 	if mamamia.Data != "mamamia" {
-		t.Logf("Error on get. Should have \"mamamia\", have %v", mamamia.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have \"mamamia\", have %v", mamamia.Data)
 	}
 	if numbers.Data != 123 {
-		t.Logf("Error on get. Should have 123, have %v", numbers.Data)
-		t.Fail()
+		t.Errorf("Error on get. Should have 123, have %v", numbers.Data)
 	}
 }
 func TestRemoveEmptyList(t *testing.T) {
@@ -159,7 +141,6 @@ func TestRemoveEmptyList(t *testing.T) {
 	_, err := list.Remove(2)
 
 	if err == nil {
-		t.Logf("Error on remove. Should have an error on empty list removal")
-		t.Fail()
+		t.Errorf("Error on remove. Should have an error on empty list removal")
 	}
 }
