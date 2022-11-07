c468269145c4a2e3d6f8109435908b84d0010fe9
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-11-07 11:10:04 +0100

    feat(pointers): added function with nil return for linked list struct

--- pointers/linkedList.go
+++ pointers/linkedList.go
@@ -9,6 +9,29 @@ type LinkedList interface {
 	Size() (int, error)
 }
 
+type linkedList struct {
+	head *Node
+}
+
 func NewLinkedList() LinkedList {
+	return &linkedList{nil}
+}
+
+func (ll *linkedList) Push(data interface{}) error {
 	return nil
+}
+func (ll *linkedList) Get(index int) (*Node, error) {
+	return nil, nil
+}
+func (ll *linkedList) Insert(data interface{}, position int) error {
+	return nil
+}
+func (ll *linkedList) Remove(index int) (*Node, error) {
+	return nil, nil
+}
+func (ll *linkedList) Search(data interface{}) (*Node, error) {
+	return nil, nil
+}
+func (ll *linkedList) Size() (int, error) {
+	return 0, nil
 }
